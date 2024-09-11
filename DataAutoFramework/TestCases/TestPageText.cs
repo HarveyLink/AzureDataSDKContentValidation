8193c9f6f2e099e19014a5d6cfa939a46a8e9d59
zedy <zedy@wicresoft.com>
2024-09-11 10:41:58 +0800

    add testcase: test blank node

--- DataAutoFramework/TestCases/TestPageText.cs
+++ DataAutoFramework/TestCases/TestPageText.cs
@@ -64,5 +64,24 @@ namespace DataAutoFramework.TestCases
 
             ClassicAssert.Zero(errorList.Count, testLink + " has wrong format" + string.Join(",", errorList));
         }
+
+        [Test]
+        [TestCaseSource(nameof(TestLinks))]
+        public void TestBlankNode(string testLink)
+        {
+            var blankNodeCount = 0;
+            var web = new HtmlWeb();
+            var doc = web.Load(testLink);
+            HtmlNodeCollection items = doc.DocumentNode.SelectNodes("//div[contains(@class, 'admonition seealso')]/ul[contains(@class, 'simple')]/li");
+            if(items != null && items.Count > 0)
+            {
+                foreach (var item in items)
+                {
+                    blankNodeCount += String.IsNullOrEmpty(item.InnerText) ? 1 : 0;
+                }
+            }
+            
+            ClassicAssert.Zero(blankNodeCount);
+        }
     }
 }
