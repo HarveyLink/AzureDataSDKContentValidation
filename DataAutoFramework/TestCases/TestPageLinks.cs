56cffaa1a331182e6ff890668aba7ad0303ffd99
zedy <zedy@wicresoft.com>
2024-09-13 10:24:11 +0800

    Add testcase: validate unparsed params

--- DataAutoFramework/TestCases/TestPageLinks.cs
+++ DataAutoFramework/TestCases/TestPageLinks.cs
@@ -23,7 +23,8 @@ namespace DataAutoFramework.TestCases
             {
                 "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-mgmt-agrifood/1.0.0b3/index.html",
                 "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-mgmt-advisor/9.0.0/azure.mgmt.advisor.operations.html",
-                "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-ai-inference/1.0.0b3/azure.ai.inference.aio.html"
+                "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-ai-inference/1.0.0b3/azure.ai.inference.aio.html",
+                "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-mixedreality-remoterendering/1.0.0b2/azure.mixedreality.remoterendering.html#"
             };
         }
         
@@ -108,9 +109,25 @@ namespace DataAutoFramework.TestCases
             var web = new HtmlWeb();
             var doc = web.Load(testLink);
             MatchCollection matches = Regex.Matches(doc.DocumentNode.SelectSingleNode("/html").InnerText, @"\[.*\]\[.*[^source]\]");
-            foreach(Match match in matches)
+            foreach (Match match in matches)
             {
                 errorList.Add(match.Value);
+            }
+            ClassicAssert.Zero(errorList.Count, string.Join("\n", errorList));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestLinks))]
+        public void TestUnparsedParams(string testLink)
+        {
+            var errorList = new List<string>();
+            var web = new HtmlWeb();
+            var doc = web.Load(testLink);
+            MatchCollection matches = Regex.Matches(doc.DocumentNode.SelectSingleNode("/html").InnerText, @"\:\S*");
+            foreach (Match match in matches)
+            {
+                if(match.Value != ":" && !match.Value.Contains("://"))
+                    errorList.Add(match.Value);
             }
             ClassicAssert.Zero(errorList.Count, string.Join("\n", errorList));
         }
