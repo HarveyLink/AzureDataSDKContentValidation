c2b3227483e3a4411bc9a5a88d4cd2101f1830c9
v-weiyding <v-weiyding@microsoft.com>
2024-10-16 13:40:03 +0800

    dev test type

--- DataAutoFramework/TestCases/TestPageType.cs
+++ DataAutoFramework/TestCases/TestPageType.cs
@@ -12,6 +12,9 @@ using DataAutoFramework.Helper;
 using System.Reflection.Emit;
 using System.Text.RegularExpressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata;
+using System.Data.Common;
 
 namespace DataAutoFramework.TestCases
 {
@@ -32,9 +35,11 @@ namespace DataAutoFramework.TestCases
                 "https://azuresdkdocs.blob.core.windows.net/$web/python/azure-mixedreality-remoterendering/1.0.0b2/azure.mixedreality.remoterendering.html#azure.mixedreality.remoterendering.RenderingSessionSize",
                 "https://learn.microsoft.com/en-us/python/api/azure-mgmt-batch/azure.mgmt.batch.models.automaticosupgradepolicy",
                 "https://learn.microsoft.com/en-us/python/api/azure-mgmt-batch/azure.mgmt.batch.models.azureresource",
-                "https://learn.microsoft.com/en-us/python/api/azure-cosmos/azure.cosmos.aio.databaseproxy"
+                "https://learn.microsoft.com/en-us/python/api/azure-cosmos/azure.cosmos.aio.databaseproxy",
+                "https://learn.microsoft.com/en-us/python/api/azure-storage-file-share/azure.storage.fileshare.shareprotocols",
+                "https://learn.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compute.v2015_06_15.operations.virtualmachinesizesoperations",
+                "https://learn.microsoft.com/en-us/python/api/azure-mgmt-advisor/azure.mgmt.advisor.aio.operations.suppressionsoperations"
             };
-
 
         }
 
@@ -48,13 +53,25 @@ namespace DataAutoFramework.TestCases
             var page = await browser.NewPageAsync();
             await page.GotoAsync(testLink);
 
-            var pyClassParamMap = await GetParamMap(page, ".py.class");
-            var pyMethodParamMap = await GetParamMap(page, ".py.method");
-            var pyClassErrorList = await ValidParamMap(pyClassParamMap, true);
-            var pyMethodErrorList = await ValidParamMap(pyMethodParamMap, false);
+            Dictionary<string, List<string>>? pyClassParamMap = null;
+            Dictionary<string, List<string>>? pyMethodParamMap = null;
 
+            if (testLink.Contains("azuresdkdocs", StringComparison.OrdinalIgnoreCase))
+            {
+                pyClassParamMap = await GetParamMap4AzureSdkDocs(page, true);
+                pyMethodParamMap = await GetParamMap4AzureSdkDocs(page, false);
+            }
+            else if (testLink.Contains("learn.microsoft", StringComparison.OrdinalIgnoreCase))
+            {
+                pyClassParamMap = await GetParamMap4LearnMicrosoft(page, true);
+                pyMethodParamMap = await GetParamMap4LearnMicrosoft(page, false);
+            }
+
+            List<string> pyClassErrorList = ValidParamMap(pyClassParamMap!, true);
+            List<string> pyMethodErrorList = ValidParamMap(pyMethodParamMap!, false);
             errorList.AddRange(pyClassErrorList);
             errorList.AddRange(pyMethodErrorList);
+
             errorList = errorList.Distinct().ToList();
 
             await browser.CloseAsync();
@@ -86,48 +103,146 @@ namespace DataAutoFramework.TestCases
             }
         }
 
-        async Task<Dictionary<string, IReadOnlyList<ILocator>>> GetParamMap(IPage page, string selector)
+        async Task<Dictionary<string, List<string>>> GetParamMap4AzureSdkDocs(IPage page, bool isClass)
         {
-            Dictionary<string, IReadOnlyList<ILocator>> paramMap = new Dictionary<string, IReadOnlyList<ILocator>>();
-            var HTMLElementList = await page.Locator(selector).AllAsync();
+            Dictionary<string, List<string>> paramMap = new Dictionary<string, List<string>>();
+
+            IReadOnlyList<ILocator>? HTMLElementList = null;
+            if (isClass)
+            {
+                HTMLElementList = await page.Locator(".py.class").AllAsync();
+            }
+            else
+            {
+                HTMLElementList = await page.Locator(".py.method").AllAsync();
+            }
+
 
             for (int i = 0; i < HTMLElementList.Count; i++)
             {
                 var HTMLElement = HTMLElementList[i];
                 var dtElement = HTMLElement.Locator("dt").Nth(0);
-                var dtId = await dtElement.GetAttributeAsync("id");
-                var paramList = await dtElement.Locator(".sig-param").AllAsync();
+                var key = await dtElement.GetAttributeAsync("id");
+                var paramLocatorsList = await dtElement.Locator(".sig-param").AllAsync();
 
-                paramMap[dtId] = paramList;
+                List<string> paramList = new List<string>();
+
+                foreach (var locator in paramLocatorsList)
+                {
+                    var innerText = await locator.InnerTextAsync();
+                    paramList.Add(innerText);
+                }
+
+                paramMap[key] = paramList;
+            }
+
+            return paramMap;
+        }
+
+        async Task<Dictionary<string, List<string>>> GetParamMap4LearnMicrosoft(IPage page, bool isClass)
+        {
+            Dictionary<string, List<string>> paramMap = new Dictionary<string, List<string>>();
+
+            IReadOnlyList<ILocator>? HTMLElementList = null;
+            if (isClass)
+            {
+                HTMLElementList = await page.Locator(".content > .wrap.has-inner-focus").AllAsync();
+            }
+            else
+            {
+                HTMLElementList = await page.Locator(".memberInfo > .wrap.has-inner-focus").AllAsync();
+            }
+
+            for (int i = 0; i < HTMLElementList.Count; i++)
+            {
+
+                var HTMLElement = HTMLElementList[i];
+                var codeText = await HTMLElement.InnerTextAsync();
+
+                var regex = new Regex(@"(?<key>.+?)\((?<params>.+?)\)");
+                var match = regex.Match(codeText);
+
+                if (!match.Success)
+                {
+                    Console.WriteLine("Ignore codeText : ");
+                    Console.WriteLine(codeText);
+                    Console.WriteLine("");
+                    continue;
+                }
+
+                string key = match.Groups["key"].Value.Trim();
+                string paramsText = match.Groups["params"].Value.Trim();
+
+                var paramList = SplitParameters(paramsText);
+
+                paramMap[key] = paramList;
             }
 
             return paramMap;
         }
 
 
-        async Task<List<string>> ValidParamMap(Dictionary<string, IReadOnlyList<ILocator>> paramMap, bool isClass)
+
+        List<string> SplitParameters(string paramsText)
+        {
+            var paramList = new List<string>();
+            int bracketCount = 0;
+            string currentParam = "";
+
+            for (int i = 0; i < paramsText.Length; i++)
+            {
+                char c = paramsText[i];
+
+                if (c == '[')
+                {
+                    bracketCount++;
+                }
+                else if (c == ']')
+                {
+                    bracketCount--;
+                }
+                else if (c == ',' && bracketCount == 0)
+                {
+                    paramList.Add(currentParam.Trim());
+                    currentParam = "";
+                    continue;
+                }
+
+                currentParam += c;
+            }
+
+            if (!string.IsNullOrWhiteSpace(currentParam))
+            {
+                paramList.Add(currentParam.Trim());
+            }
+
+            return paramList;
+        }
+
+
+        List<string> ValidParamMap(Dictionary<string, List<string>> paramMap, bool isClass)
         {
 
             var errorList = new List<string>();
 
             foreach (var item in paramMap)
             {
-                string dtId = item.Key;
+                string key = item.Key;
                 var paramList = item.Value;
 
                 if (isClass && paramList.Count == 0)
                 {
-                    errorList.Add($"Empty argument : {dtId}");
-                    Console.WriteLine($"Empty argument : {dtId}");
+                    errorList.Add($"Empty argument : {key}");
+                    Console.WriteLine($"Empty argument : {key}");
                 }
 
                 for (int i = 0; i < paramList.Count; i++)
                 {
-                    var text = await paramList[i].InnerTextAsync();
+                    var text = paramList[i];
 
                     if (!IsCorrectTypeAnnotation(text))
                     {
-                        errorList.Add($"Missing type annotations : {dtId}");
+                        errorList.Add($"Missing type annotations : {key}");
                         Console.WriteLine($"type argument : {text}");
                     }
                 }
